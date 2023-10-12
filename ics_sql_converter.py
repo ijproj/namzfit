82c101555f57d81fab358bf52630a0810e27aa47
Ijeoma <ijeoma.ehigiator@hotmail.co.uk>
2023-10-13 00:24:39 +0100

    database connection

--- ics_sql_converter.py
+++ ics_sql_converter.py
@@ -5,6 +5,8 @@ from icalendar import Calendar
 import pyodbc
 from dotenv import load_dotenv
 import os
+from sqlalchemy import create_engine, URL, text, MetaData, Table, select
+# from sqlalchemy.engine.url import URL
 
 # upload calendar as an ics file to block out availability
 path_to_ics_file = "/Users/ijeomaehigiator/Desktop/availability.ics"
@@ -36,11 +38,11 @@ day_end = day_end.replace(hour=hour_end)
 session_begins = pd.period_range(start=day_start, end=day_end, freq='1H').to_timestamp()
 session_begins = session_begins[(session_begins.hour >= hour_start) & (session_begins.hour < hour_end)]
 session_ends = session_begins + np.timedelta64(1, 'h')
-time_slots = pd.DataFrame({'start_time':session_begins, 'end_time':session_ends})
+timeslots = pd.DataFrame({'start_time':session_begins, 'end_time':session_ends})
 
-time_slots = time_slots[~time_slots.start_time.isin(bookings.start_date)]
-time_slots = time_slots[~time_slots.end_time.isin(bookings.end_date)]
-time_slots['available'] = 1
+timeslots = timeslots[~timeslots.start_time.isin(bookings.start_date)]
+timeslots = timeslots[~timeslots.end_time.isin(bookings.end_date)]
+timeslots['available'] = 1
 
 # print(time_slots)
 load_dotenv()
@@ -54,9 +56,23 @@ connection_str = (
     f'UID={username};'
     f'PWD={password};'
 )
+# engine_str = f'mysql+pyodbc://{username}:{password}@localhost:3306/namzfit'
 
+url_object = URL.create(
+    "mysql+pyodbc",
+    username=username,
+    password=password,  # plain (unescaped) text
+    host="localhost",
+    port=3306,
+    database="namzfit",
+    query=dict(driver='MySQL ODBC 8.1 ANSI Driver', MULTI_HOST=1),
+)
 # Establish a connection
-connection = pyodbc.connect(connection_str)
+# connection = pyodbc.connect(connection_str)
+engine = create_engine(url_object)
+metadata = MetaData()
 
+db_timeslots = Table('timeslots', metadata, autoload=True, autoload_with=engine)
+# timeslots.to_sql('timeslots', engine, if_exists='append', index=False)
 # edit below to use SQLAlchemy.
 # pd.read_sql_query('''select * from bookings''', connection) 
