34cd3c02f937721d0e7ec1c02c49fc92423f9c31
Ijeoma <ijeoma.ehigiator@hotmail.co.uk>
2023-10-18 21:56:06 +0100

    updating database

--- ics_sql_converter.py
+++ ics_sql_converter.py
@@ -5,8 +5,8 @@ from icalendar import Calendar
 import pyodbc
 from dotenv import load_dotenv
 import os
-from sqlalchemy import create_engine, URL, text, MetaData, Table, select
-# from sqlalchemy.engine.url import URL
+from sqlalchemy import create_engine, URL, text, MetaData, select
+from sqlalchemy.orm import sessionmaker
 
 # upload calendar as an ics file to block out availability
 path_to_ics_file = "/Users/ijeomaehigiator/Desktop/availability.ics"
@@ -22,33 +22,33 @@ for booking in calendar.walk('vevent'):
     event.availability = [0]
     bookings = pd.concat([bookings, event], ignore_index=True)
 bookings
+
 # create available slots
 hour_start = 9
 hour_end = 22
 today = pd.Timestamp('today')
-# year_start = pd.Timestamp('today').floor('d').year
 month_today = pd.Timestamp('today').floor('d').month
 
 day_start = pd.Timestamp('today') + np.timedelta64(1, 'M')
-day_start = day_start.replace(day=1, hour=hour_start)
+day_start = day_start.replace(day=1, hour=hour_start).floor('h')
 day_end = day_start + np.timedelta64(3, 'M')
-day_end = day_end.replace(hour=hour_end)
+day_end = day_end.replace(hour=hour_end).floor('h')
 
 
 session_begins = pd.period_range(start=day_start, end=day_end, freq='1H').to_timestamp()
 session_begins = session_begins[(session_begins.hour >= hour_start) & (session_begins.hour < hour_end)]
 session_ends = session_begins + np.timedelta64(1, 'h')
-timeslots = pd.DataFrame({'start_time':session_begins, 'end_time':session_ends})
+timeslots = pd.DataFrame({'start_date':session_begins, 'end_date':session_ends})
 
-timeslots = timeslots[~timeslots.start_time.isin(bookings.start_date)]
-timeslots = timeslots[~timeslots.end_time.isin(bookings.end_date)]
-timeslots['available'] = 1
+timeslots = timeslots[~timeslots.start_date.isin(bookings.start_date)]
+timeslots = timeslots[~timeslots.end_date.isin(bookings.end_date)]
+timeslots['availability'] = 1
 
-# print(time_slots)
+
 load_dotenv()
 password = os.getenv("PASSWORD")
 username = os.getenv("USERNAME")
-# Create a connection string
+
 connection_str = (
     r'DRIVER={MySQL ODBC 8.1 ANSI Driver};'
     r'SERVER=localhost;'
@@ -56,23 +56,27 @@ connection_str = (
     f'UID={username};'
     f'PWD={password};'
 )
-# engine_str = f'mysql+pyodbc://{username}:{password}@localhost:3306/namzfit'
+
 
 url_object = URL.create(
     "mysql+pyodbc",
     username=username,
-    password=password,  # plain (unescaped) text
+    password=password,  
     host="localhost",
     port=3306,
     database="namzfit",
-    query=dict(driver='MySQL ODBC 8.1 ANSI Driver', MULTI_HOST=1),
+    query=dict(driver='MySQL ODBC 8.1 ANSI Driver', MULTI_HOST="1"),
 )
-# Establish a connection
-# connection = pyodbc.connect(connection_str)
+
 engine = create_engine(url_object)
+
 metadata = MetaData()
 
-db_timeslots = Table('timeslots', metadata, autoload=True, autoload_with=engine)
-# timeslots.to_sql('timeslots', engine, if_exists='append', index=False)
-# edit below to use SQLAlchemy.
-# pd.read_sql_query('''select * from bookings''', connection) 
+query = f"SELECT * FROM timeslots WHERE start_date >= '{day_start}'"
+existing_timeslots = pd.read_sql_query(query, engine)
+
+unique_timeslots = timeslots[~timeslots.start_date.isin(existing_timeslots.start_date)]
+
+
+unique_timeslots.to_sql('timeslots', con=engine, if_exists='append', index=False)
+
